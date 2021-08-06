a862dc936621d6de2ded23324af791ed825bb4ac
Bhavyabansall <12bhavya.gupta@gmail.com>
2021-08-07 00:04:51 +0530

    6

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/bhavya'),
+      dir: require('path').join(__dirname, './coverage/Bhavya'),
       subdir: '.',
       reporters: [
         { type: 'html' },
